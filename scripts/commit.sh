de904378e521e0ed2717de2cba716e75b6716c8c
Łukasz Adamczak <lukasz@czak.pl>
2020-03-19 21:47:21 +0100

    Scribble $weekday lines

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -2,6 +2,11 @@
 git config --global user.email $GIT_USER_EMAIL
 git config --global user.name $GIT_USER_NAME
 git clone $REPO_URL --branch $BRANCH_NAME --single-branch /checkout
-scribble index.html
-git commit -am "I accidentally a line"
-git push
+
+weekday=$(date +'%u')
+for i in `seq 1 $weekday`; do
+  scribble index.html
+  git commit -am "I accidentally a line #$i"
+  git push
+  sleep 1
+done
