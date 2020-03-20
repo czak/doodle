0d95541c45da1c56f00ffd03254ed447171b8baf
Łukasz <lukasz@czak.pl>
2020-03-20 20:22:09 +0100

    Start estimate in Go

--- tools-go/cmd/estimate/estimate.go
+++ tools-go/cmd/estimate/estimate.go
@@ -4,22 +4,33 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/antchfx/htmlquery"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatal("Bad!")
+		log.Fatal("usage: estimate username")
 	}
 
 	username := os.Args[1]
 
+	fmt.Println(getCurrent(username))
+}
+
+func getCurrent(username string) int {
 	doc, err := htmlquery.LoadURL("https://github.com/" + username)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	rect := htmlquery.FindOne(doc, "//g[last()]/rect[last()]")
-	fmt.Println(htmlquery.SelectAttr(rect, "data-count"))
+	attr := htmlquery.SelectAttr(rect, "data-count")
+	n, err := strconv.Atoi(attr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return n
 }
