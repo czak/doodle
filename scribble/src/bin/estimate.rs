426b067edaec235987d55a8cbf7521071bd9b736
Łukasz Adamczak <lukasz@czak.pl>
2020-03-20 11:22:37 +0100

    Some cleanup

--- scribble/src/bin/estimate.rs
+++ scribble/src/bin/estimate.rs
@@ -1,25 +1,33 @@
 use scraper::{Html, Selector};
 
 fn main() {
-    let username = match std::env::args().nth(1) {
-        Some(s) => s,
-        None => {
-            eprintln!("must specify username");
-            std::process::exit(1);
-        }
-    };
+    let username = std::env::args().nth(1).expect("must specify username");
 
-    // let html = {
-    //     let url = format!("https://github.com/{}", username);
-    //     let resp = reqwest::blocking::get(&url).unwrap();
-    //     resp.text().unwrap()
-    // };
+    let current = get_current(&username);
 
-    let html = include_str!("../../czak.html");
+    println!("{}", current);
+}
 
-    let document = Html::parse_document(&html);
+fn get_current(username: &str) -> u32 {
+    let document = Html::parse_document(&get_profile(username));
     let selector = Selector::parse("g g:last-of-type rect:last-child").unwrap();
 
-    let rect = document.select(&selector).next().unwrap();
-    println!("{}", rect.value().attr("data-count").unwrap());
+    let rect = document.select(&selector).next().expect("rect not found");
+    rect.value()
+        .attr("data-count")
+        .expect("data-count attribute missing")
+        .parse()
+        .expect("unable to parse data-count to u32")
+}
+
+#[cfg(debug_assertions)]
+fn get_profile(_username: &str) -> String {
+    String::from(include_str!("../../data/czak.html"))
+}
+
+#[cfg(not(debug_assertions))]
+fn get_profile(username: &str) -> String {
+    let url = format!("https://github.com/{}", username);
+    let resp = reqwest::blocking::get(&url).expect("failed to get url");
+    resp.text().unwrap()
 }
