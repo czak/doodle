2cbf8b02a735d99f1d57d110ca96d440e7d23e1d
Łukasz <lukasz@czak.pl>
2020-03-20 12:32:25 +0100

    Use estimate in scripts/commit.sh

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -3,8 +3,8 @@ git config --global user.email $GIT_USER_EMAIL
 git config --global user.name $GIT_USER_NAME
 git clone $REPO_URL --branch $BRANCH_NAME --single-branch /checkout
 
-weekday=$(date +'%u')
-for i in `seq 1 $weekday`; do
+target=$(estimate $USERNAME)
+for i in `seq $target`; do
   scribble index.html
   git commit -am "I accidentally a line #$i"
   git push
