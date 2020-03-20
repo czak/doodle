39e44e118c05bc0b75564ab976cb4f8307507428
Łukasz <lukasz@czak.pl>
2020-03-20 20:47:55 +0100

    Working estimate in go

--- tools-go/cmd/estimate/estimate.go
+++ tools-go/cmd/estimate/estimate.go
@@ -5,8 +5,14 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/antchfx/htmlquery"
+)
+
+var (
+	pattern    []int = []int{0, 8, 16, 24, 32, 40, 48, 54, 60, 66, 72, 78}
+	patternLen int   = 84
 )
 
 func main() {
@@ -14,9 +20,25 @@ func main() {
 		log.Fatal("usage: estimate username")
 	}
 
-	username := os.Args[1]
+	current := getCurrent(os.Args[1])
+	target := getTarget()
 
-	fmt.Println(getCurrent(username))
+	if target > current {
+		fmt.Println(target - current)
+	} else {
+		fmt.Println(0)
+	}
+}
+
+func getTarget() int {
+	start := time.Date(2020, 1, 5, 0, 0, 0, 0, time.UTC)
+	offset := (int(time.Since(start).Hours()) / 24) % patternLen
+
+	if contains(pattern, offset) {
+		return 10
+	} else {
+		return 4
+	}
 }
 
 func getCurrent(username string) int {
@@ -33,4 +55,13 @@ func getCurrent(username string) int {
 	}
 
 	return n
+}
+
+func contains(s []int, n int) bool {
+	for _, m := range s {
+		if m == n {
+			return true
+		}
+	}
+	return false
 }
